e859467ea7850c41bc4f103595cd0b200d07f76e
Marck-V <42965997+Marck-V@users.noreply.github.com>
2023-09-30 21:33:58 -0700

    ?

--- player.gd
+++ player.gd
@@ -20,6 +20,8 @@ func _physics_process(delta):
 	else :
 		animated_sprite_2d.play("Jump")
 	
+	if Input.is_action_just_pressed("swap"):
+		swapped()
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():		
 		velocity.y = JUMP_VELOCITY
@@ -46,4 +48,5 @@ func _on_reset_box_body_entered(body):
 func reset_spawn():
 	player.position = spawn_point.position
 	
-
+func swapped():
+	emit_signal("swap_pressed")
